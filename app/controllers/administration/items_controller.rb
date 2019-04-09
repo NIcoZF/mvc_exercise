012c6f6cb177817e21796d91b8fcd4cdeccd28d7
NIcoZF <nico-7@hotmail.fr>
2019-04-09 17:02:06 +0200

    add update method to administration item controller

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -8,6 +8,13 @@ module Administration
     end
 
     def update
+      @item = Item.find(params[:id])
+
+      if params[:item][:discount_percentage] == "0"
+        @item.update(has_discount: false, discount_percentage: [:item][:discount_percentage])
+      else
+        @item.update(has_discount: true, discount_percentage: [:item][:discount_percentage])
+      end
       redirect_to administration_items_path
     end
   end
