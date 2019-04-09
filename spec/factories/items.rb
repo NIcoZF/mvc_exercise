0bad408a795cc658b7003b38ea816b17634b7156
NIcoZF <nico-7@hotmail.fr>
2019-04-09 12:21:24 +0200

    add db testing and modify factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -16,6 +16,7 @@ FactoryBot.define do
   factory :item do
     original_price      { Faker::Number.decimal(2) }
     has_discount        { Faker::Boolean.boolean }
+    discount_percentage { Faker::Number.number(2) }
 
     trait :with_discount do
       has_discount { true }
